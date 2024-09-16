9ec1dfe0c592f146bc7b66e40e72103774847750
andrii01001 <andrii.danyliuk87@gmail.com>
2024-09-16 22:58:32 +0300

    Saving Dato to Files and Databases

--- toscrape/toscrape/settings.py
+++ toscrape/toscrape/settings.py
@@ -64,6 +64,8 @@ ROBOTSTXT_OBEY = True
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
    "toscrape.pipelines.ToscrapePipeline": 300,
+   "toscrape.pipelines.SaveToMySQLPipeline": 400
+   
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
